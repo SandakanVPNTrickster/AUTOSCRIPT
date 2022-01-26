81ad3f5d4672a989ed3150a723a06f47aea0a39b
jinGGo007 <73293452+jinGGo007@users.noreply.github.com>
2022-01-26 18:42:23 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,6 +11,13 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 
+#Disable ipv6
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+
+#Install Update
+apt -y update
+apt install -y bzip2 gzip coreutils screen curl
 
 # Script Access 
 MYIP=$(wget -qO- icanhazip.com);
